90be8ebb96605125b41be04947c8fc22da1a7913
softwaressale <waterdrop198@gmail.com>
2025-03-22 20:59:51 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('False')
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     'stf-trial.onrender.com',  # Render hostname
